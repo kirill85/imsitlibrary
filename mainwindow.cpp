f2bd2b5ecf4cee8a775067fa171d21371485ba55
Alexey Shmakov <razzeeyy@gmail.com>
2015-06-16 22:19:23 +0300

    form showing code for mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "publisher.h"
+#include "bookinfo.h"
+#include "userinfo.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -16,20 +19,31 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_publishButton_clicked()
 {
-
+    Publisher publisher_info = new Publisher(parent);
+    publisher_info.show();
+    this->hide();
 }
 
 void MainWindow::on_eLibraryButton_clicked()
 {
+    BookInfo book_info = new BookInfo(parent);
+    book_info.show();
 
+    UserInfo user_info = new UserInfo(parent);
+    user_info.show();
+    this->hide();
 }
 
 void MainWindow::on_bookInfoButton_clicked()
 {
-
+    BookInfo book_info = new BookInfo(parent);
+    book_info.show();
+    this->hide();
 }
 
 void MainWindow::on_aboutReaderButton_clicked()
 {
-
+    UserInfo user_info = new UserInfo(parent);
+    user_info.show();
+    this->hide();
 }
